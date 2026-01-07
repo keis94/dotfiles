2ddf4ade506f743620bd3230b11609b171130050
keis94 <keis.vivi@gmail.com>
2026-01-08 01:46:08 +0900

    fix: add symbol of lazy.nvim to LuaLS

--- nvim/after/lsp/lua_ls.lua
+++ nvim/after/lsp/lua_ls.lua
@@ -1,11 +1,37 @@
+---@param names string[]
+---@return string[]
+local function get_plugin_paths(names)
+  local plugins = require("lazy.core.config").plugins
+  local paths = {}
+  for _, name in ipairs(names) do
+    if plugins[name] then
+      table.insert(paths, plugins[name].dir .. "/lua")
+    else
+      vim.notify("Invalid plugin name: " .. name)
+    end
+  end
+  return paths
+end
+
+---@param plugins string[]
+---@return string[]
+local function library(plugins)
+  local paths = get_plugin_paths(plugins)
+  table.insert(paths, vim.fn.stdpath("config") .. "/lua")
+  table.insert(paths, vim.env.VIMRUNTIME .. "/lua")
+  table.insert(paths, "${3rd}/luv/library")
+  table.insert(paths, "${3rd}/busted/library")
+  table.insert(paths, "${3rd}/luassert/library")
+  return paths
+end
+
 ---@type vim.lsp.Config
 return {
   settings = {
     Lua = {
       workspace = {
-        library = {
-          vim.env.VIMRUNTIME .. "/lua",
-        },
+        library = library({ "lazy.nvim" }),
+        checkThirdParty = "Disable",
       },
     },
   },
