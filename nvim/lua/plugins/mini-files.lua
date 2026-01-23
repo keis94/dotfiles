17710be63cc3a81c0b1ce06999ecddec39b0a4ca
keis94 <keis.vivi@gmail.com>
2026-01-23 23:11:08 +0900

    feat(nvim): add split keybindings to mini.files

--- nvim/lua/plugins/mini-files.lua
+++ nvim/lua/plugins/mini-files.lua
@@ -1,3 +1,4 @@
+---@module "lazy"
 ---@type LazyPluginSpec
 return {
   "nvim-mini/mini.files",
@@ -15,4 +16,34 @@ return {
     },
   },
   opts = {},
+  config = function(_, opts)
+    require('mini.files').setup(opts)
+
+    local map_split = function(buf_id, key, direction)
+      local action = function()
+        local MiniFiles = require('mini.files')
+        -- Make new window and set it as target
+        local cur_target = MiniFiles.get_explorer_state().target_window
+        local new_target = vim.api.nvim_win_call(cur_target, function()
+          vim.cmd(direction .. ' split')
+          return vim.api.nvim_get_current_win()
+        end)
+
+        MiniFiles.set_target_window(new_target)
+      end
+
+      local desc = 'Split ' .. direction
+      vim.keymap.set('n', key, action, { buffer = buf_id, desc = desc })
+    end
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'MiniFilesBufferCreate',
+      callback = function(args)
+        local buf_id = args.data.buf_id
+        map_split(buf_id, '<C-s>', 'belowright horizontal')
+        map_split(buf_id, '<C-v>', 'belowright vertical')
+        map_split(buf_id, '<C-t>', 'tab')
+      end,
+    })
+  end
 }
