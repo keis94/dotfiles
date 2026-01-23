707d841a988ff08237a87d2b985327ae2c6e0edc
keis94 <keis.vivi@gmail.com>
2026-01-23 14:54:03 +0900

    fix: completion keymap

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,3 +1,5 @@
+---@module 'lazy'
+---@type LazyPluginSpec
 return {
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
@@ -25,7 +27,22 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'default' },
+    keymap = {
+      preset = 'default',
+      ['C-space'] = {},
+      ['<C-e>'] = { 'show', 'show_documentation', 'hide_documentation', 'fallback' },
+      ['<Tab>'] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        'snippet_forward',
+        'fallback'
+      },
+    },
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
