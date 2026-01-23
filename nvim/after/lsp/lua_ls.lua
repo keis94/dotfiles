c2c6104e6b904edc94d4996f8f44a73ce764611e
keis94 <keis.vivi@gmail.com>
2026-01-23 22:52:46 +0900

    fix(nvim): add plugin path to library (lua_ls)

--- nvim/after/lsp/lua_ls.lua
+++ nvim/after/lsp/lua_ls.lua
@@ -1,36 +1,10 @@
----@param names string[]
----@return string[]
-local function get_plugin_paths(names)
-  local plugins = require("lazy.core.config").plugins
-  local paths = {}
-  for _, name in ipairs(names) do
-    if plugins[name] then
-      table.insert(paths, plugins[name].dir .. "/lua")
-    else
-      vim.notify("Invalid plugin name: " .. name)
-    end
-  end
-  return paths
-end
-
----@param plugins string[]
----@return string[]
-local function library(plugins)
-  local paths = get_plugin_paths(plugins)
-  table.insert(paths, vim.fn.stdpath("config") .. "/lua")
-  table.insert(paths, vim.env.VIMRUNTIME .. "/lua")
-  table.insert(paths, "${3rd}/luv/library")
-  table.insert(paths, "${3rd}/busted/library")
-  table.insert(paths, "${3rd}/luassert/library")
-  return paths
-end
-
 ---@type vim.lsp.Config
 return {
   settings = {
     Lua = {
       workspace = {
-        library = library({ "lazy.nvim" }),
+        -- Loading everything slows it down, but lazydev.nvim broke suddenly, so this is workaround.
+        library = vim.api.nvim_get_runtime_file("", true),
         checkThirdParty = "Disable",
       },
     },
